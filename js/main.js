dbbe1bb67eeda593b51d777e29ae4e0c4db2f1bb
Jordan Escoto <jordanescoto@gmail.com>
2014-05-02 16:40:58 -0400

    Updated bus selection

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var linesAPI = 'http://173.224.125.206:8803/OperadorServiceRest/linhas';
 var line_Data = [];
-var way_val = "";
+var way_val, stop_val = 0;
 
 function getLines(){
 	$.getJSON(linesAPI, function(json) {
@@ -8,15 +8,12 @@ function getLines(){
 	$.each(lines, function(index, value) {
 		var line_name = lines[index].nome;
 		var line_id = lines[index].id;
-		console.log(line_name);
 		$('#lines-sel').append($('<option>')
 			.text(line_name)
 			.attr('value', line_id)
 		);
 	});
 
-
-	console.log('line id :', lines);
 	});
 }; 
 
@@ -35,14 +32,28 @@ function getWay(way_val) {
 			.attr('value', ways_id)
 		);
 	});
-	console.log(wayAPI);
 	});
 };
 
-function GetStop() {
+function getStop(stop_val) {
+	var stopAPI = "http://173.224.125.206:8803/OperadorServiceRest/pontos/" + stop_val;
 
-}
-
+	$.getJSON(stopAPI, function(json) {
+	var stop = json.pontos;
+	$.each(stop, function(index, value) {
+		var stop_name = stop[index].nome;
+		var stop_id = stop[index].id;
+		var stop_tipo = stop[index].tipo;
+		var stop_lat = stop[index].latitude;
+		var stop_long = stop[index].longitude;
+		
+		$('#lines-stop').append($('<option>')
+			.text(stop_name)
+			.attr('value', stop_id)
+		);
+	});
+	});
+};
 
 
 $("#lines-sel").change(function() {
@@ -57,7 +68,26 @@ $("#lines-sel").change(function() {
 	 };
 });
 
+$("#lines-way").change(function() {
+	var stop_val = $("#lines-way option:selected").val();
+	var stop = stop_val;
+	 if (stop_val > 0) {
+	 	$("#lines-stop option[value!= '0']").remove();
+	 	$("#lines-stop").removeAttr('disabled');
+	 	getStop(stop);
+	 } else {
+	 	console.log("Select not active");
+	 };
+});
+
+$("#lines-stop").change(function() {
+	$("#run-btn").removeAttr('disabled');
+});
 
 $("#load-sel").click(function() {
 	getLines();
+});
+
+$("#run-btn").click(function() {
+	$("#lineModal").modal("hide");
 });
