c21a51f5b576e89ddf6191278ba71b5216dbbc98
Jordan <jordanescoto@gmail.com>
2014-05-23 19:40:49 -0400

    Added map panel

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-var host = "http://173.224.125.206:8803/"
+var host = 'http://173.224.125.206:8803/';
 var linesAPI = 'http://173.224.125.206:8803/OperadorServiceRest/linhas';
 var line_Data = [];
 var way_val, stop_val = 0;
 var meters = 0;
 var timing = 0;
 var updateTimes;
-
+var buslist = [];
+var sel_bus_lat;
 
 function getLines(){
 	$.getJSON(linesAPI, function(json) {
@@ -21,7 +22,7 @@ function getLines(){
 	});
 
 	});
-}; 
+}
 
 function getWay(way_val) {
 	var wayAPI = host + "OperadorServiceRest/sentidos/" + way_val;
@@ -41,7 +42,7 @@ function getWay(way_val) {
 	});
 	});
 	return way_val;
-};
+}
 
 function getStop(stop_val) {
 	var stopAPI = host+ "OperadorServiceRest/pontos/" + stop_val;
@@ -56,21 +57,17 @@ function getStop(stop_val) {
 		var stop_long = stop[index].longitude;
 		var stop_fac = stop[index].patternFraction;
 		
-		$('#lines-stop').append($('<option>')
-			.text(stop_name)
-			.attr('value', stop_id)
-			.attr('pat', stop_fac)
-		);
+		$('#lines-stop').append($('<option>').text(stop_name).attr('value', stop_id).attr('pat', stop_fac));
 
 	});
 	});
-};
+}
 
 
 
 function update() {
 	
-	clearInterval(updateTimes);	
+	clearInterval(updateTimes);
 
 	var way_sel = $("#lines-way option:selected").val();
 	var line_sel = $("#lines-sel option:selected").val();
@@ -79,7 +76,7 @@ function update() {
 	var stop_time_API = host + "OperadorServiceRest/sentido/" + line_sel + "/" + way_sel + "/tempomedio";
 
 	$.getJSON(stop_sel_API, function(json) {
-		meters = json.int
+		meters = json.int;
 		console.log("meters: " + meters);
 		caldist = calDis(meters, stop_pat);
 	
@@ -100,77 +97,94 @@ function update() {
 //	caltime = calTime(timing, stop_pat);
 //	caldist = calDis(meters, stop_pat);
 //	getBuses(line_sel, way_sel, stop_pat, caltime, caldist);
-};
+}
 
 function getBuses(line, dir, pat, caltime, caldist) {
- var getBus_API = host + "OperadorServiceRest/veiculosDaLinha/" + line + "/" + dir;
- $.getJSON(getBus_API, function(json) {
-  var bus = json.veiculos.reverse();
-  console.log(bus);
- $("#bus-list").empty();
- $.each(bus, function(index, value) {
-  var bus_pat = bus[index].patternFraction;
-  var bus_id = bus[index].id;
-  var bus_code = bus[index].codigo;
-  var bus_lat = bus[index].latitude;
-  var bus_lng = bus[index].longitude;
+	var getBus_API = host + "OperadorServiceRest/veiculosDaLinha/" + line + "/" + dir;
+	$.getJSON(getBus_API, function(json) {
+		var bus = json.veiculos.reverse();
+		console.log(bus);
+		$("#bus-list").empty();
+	$.each(bus, function(index, value) {
+		var bus_pat = bus[index].patternFraction;
+		var bus_id = bus[index].id;
+		var bus_code = bus[index].codigo;
+		var bus_lat = bus[index].latitude;
+		var bus_lng = bus[index].longitude;
 
-  console.log(caltime);
-  if (bus_pat < pat) {
-   var time2next = (caltime - ((bus_pat*timing)/60.0));
-   var time = Math.abs(time2next) >>> 0;
-   console.log(time);
-   if (time == 0) {
-   		time = "Now";
-   } else if (time == 1) {
-   		time = time + " min"
-   } else {
-   		time = time + " mins"
-   }
-
-   var appended = $('<div class="bus-box"><a href="map.html"><div class="bus-time"><div class="bus-box-left pull-left"><p>'+ time +'</p><img src="img/bus.png" height="40px" width="auto"></div></div><div class="bus-right pull-left"><div class="bus-line"><p class="vehicle-title">' + bus_code + '</p><img src="img/loc.png" width="10px" height="auto" class="topminus5"><span class="geo">Current Location</span></div></div></a></div>');
-
-   $("#bus-list").append(appended);
-   appended.geocoder(bus_lat, bus_lng); 
-  }
-  });
- });
+		console.log(bus_lng);
+		if (bus_pat < pat) {
+			var time2next = (caltime - ((bus_pat*timing)/60.0));
+			var time = Math.abs(time2next) >>> 0;
+			var currentBus = [bus_lat,bus_lng];
+			buslist.push(currentBus);
+			console.log(buslist);
+				if (time === 0) {
+				time = "Now";
+				} else if (time == 1) {
+					time = time + " min";
+				} else {
+					time = time + " mins";
+				}
+			var appended = $('<div class="bus-box" data-toggle="modal" data-target="#mapModal" lat=' + '"' + bus_lat + '"' + " " +'lng=' + '"' + bus_lng + '"><div class="bus-time"><div class="bus-box-left pull-left"><p>'+ time +'</p><img src="img/bus.png" height="40px" width="auto"></div></div><div class="bus-right pull-left"><div class="bus-line"><p class="vehicle-title">' + bus_code + '</p><img src="img/loc.png" width="10px" height="auto" class="topminus5"><span class="geo">Current Location</span></div></div></div>');
+			$("#bus-list").append(appended);
+			appended.geocoder(bus_lat, bus_lng);
+		}
+	}
+	);
+	});
 }
 
 function calTime(value1, value2) {
 	var time_to_busStop = (value1*value2)/60.0;
 	return time_to_busStop;
-};
+}
 
 function calDis(value1, value2) {
 	var dis_to_busStop = (value1*value2)/1000.0;
 	return dis_to_busStop;
 }
 
+function maptheBus(lat, lng) {
+	var busCord = new google.maps.LatLng(lat,lng);
+	var mapOptions = {
+		zoom: 6,
+		center: busCord,
+		mapTypeId: google.maps.MapTypeId.Roadmap
+	};
+
+	var map = new google.maps.Map(document.getElementById('bus-map'), mapOptions);
+	var marker = new google.maps.Marker({
+      position: busCord,
+      map: map
+  });
+}
+
+
 $("#lines-sel").change(function() {
 	var way_val = $("#lines-sel option:selected").val();
 	var way = way_val;
-	 if (way_val > 0) {
-	 	$("#lines-way option[value!= '0']").remove();
-	 	$("#lines-way").removeAttr('disabled');
-	 	getWay(way_val);
-	 } else {
-	 	console.log("Select not active");
-	 };
-	 return way_val;
+	if (way_val > 0) {
+		$("#lines-way option[value!= '0']").remove();
+		$("#lines-way").removeAttr('disabled');
+		getWay(way_val);
+	} else {
+		console.log("Select not active");
+	}
+	return way_val;
 });
 
 $("#lines-way").change(function() {
 	var stop_val = $("#lines-way option:selected").val();
 	var stop = stop_val;
-	 if (stop_val > 0) {
-	 	$("#lines-stop option[value!= '0']").remove();
-	 	$("#lines-stop").removeAttr('disabled');
-	 	getStop(stop);
-	 } else {
-	 	console.log("Select not active");
-	 };
-	 return stop_val;
+	if (stop_val > 0) {
+		$("#lines-stop option[value!= '0']").remove();
+		$("#lines-stop").removeAttr('disabled');
+		getStop(stop);
+	} else {
+		console.log("Select not active");
+	}
+	return stop_val;
 });
 
 
@@ -187,7 +201,7 @@ $("#run-btn").click(function() {
 	update();
 });
 
-// side menu
+
 $("body").click(function(e) {
 	if(e.target != sidr) {
 	$.sidr("close");
@@ -196,6 +210,9 @@ $("body").click(function(e) {
 	}
 });
 
-
-
-
+$('body').on('click','.bus-box', function(e) {
+	var sel_bus_lat = $(this).attr("lat");
+	var sel_bus_lng	= $(this).attr("lng");
+	maptheBus(sel_bus_lat, sel_bus_lng);
+	console.log(sel_bus_lat);
+	});
